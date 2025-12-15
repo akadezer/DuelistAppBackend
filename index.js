ee90855c257b0480509f776ed23bf32da3c52a98
Aleksandar M. <aleksandarmaksimovic257@gmail.com>
2025-12-15 23:10:57 +0100

    DAB-1: implemented body-parser to extract json data from body

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3000;
 const userModel = require('./models/user.model').userModel;
 const userRoutes = require('./routes/user.routes').userRoutes;
 
+app.use(express.json());
 app.use(userRoutes);
 
 app.post('/', (req, res, next) => {
