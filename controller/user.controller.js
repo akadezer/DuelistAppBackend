dba28b8c0bc1d68e51b3c01b1ad0c343b1803b56
Aleksandar M. <aleksandarmaksimovic257@gmail.com>
2025-12-16 19:06:03 +0100

    #2: implemented dummy create User function

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,3 +8,19 @@ exports.getUser = (req, res,next) => {
     })
     .catch(err => {console.log(err)});
 };
+
+exports.createUser = (req, res,next) => {
+    userModel.create({
+        DuelantenId: "1",
+        username: "Yugi Muto",
+        email: "yugi@example.com",
+        password: "exodia",
+        DeckIds: [],
+        SammlungId: null,
+        FriendlistId: null})
+        .then(result => {
+            res.status(200);
+            res.send('User created successfully');
+        })
+        .catch(err => {console.log(err)});
+    }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const userRoutes = express.Router();
 const getUser = require('../controller/user.controller').getUser;
+const createUser = require('../controller/user.controller').createUser;
 
 userRoutes.get('/user',getUser);
+userRoutes.post('/user',createUser);
 
 exports.userRoutes = userRoutes;
