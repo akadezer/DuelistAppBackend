8418269ce66acb5d9f51be95c0633a27843edcef
Aleksandar M. <aleksandarmaksimovic257@gmail.com>
2026-03-01 14:22:06 +0100

    changed attribute names to eng

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -2,12 +2,12 @@ const mongoose = require('../database').mongoose;
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    DuelantenId: { type: String, required: true, unique: true },
+    DuelistId: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     DeckIds : [{ type: mongoose.Schema.Types.ObjectId }],
-    SammlungId: { type: mongoose.Schema.Types.ObjectId },
+    CollectionId: { type: mongoose.Schema.Types.ObjectId },
     FriendlistId: { type: mongoose.Schema.Types.ObjectId }
 });
 
